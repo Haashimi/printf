5be314dc308e9880c5a4a792a6c1510509becd61
haashimi <mohameddiini@outlook.com>
2023-06-27 10:27:04 -0700

    get_size.c

---
+++ get_size.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+
+get_size - Calculates the size to cast the argument
+
+@format: Formatted string in which to print the arguments
+
+@index: Pointer to the current index in the format string
+
+Return: Size of the argument.
+*/
+int get_size(const char *format, int *index)
+{
+int current_index = *index + 1;
+int size = 0;
+
+if (format[current_index] == 'l')
+size = S_LONG;
+else if (format[current_index] == 'h')
+size = S_SHORT;
+
+if (size == 0)
+*index = current_index - 1;
+else
+*index = current_index;
+
+return size;
+}
